5157347826604010c5a4932be6bcaa402ff94be8
harpitha12345 <harpithadevops@gmail.com>
2025-03-09 18:39:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-Provider "aws" {
-region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
+
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-bucket-123456789"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
+
+
+
+
+
